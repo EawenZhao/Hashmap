c8cc515418a97477fa2ed3730a916fb7a9687bdb
zyh19 <zyh1999zyh@yeah.net>
2022-12-30 02:21:07 +0800

    Initial commit

---
+++ driver.h
@@ -0,0 +1,34 @@
+#include <fstream>     // declares << and >> for files
+#include <string>      // declares the string class
+#include "hash_map.h"    // declares and defined the hash_map class
+
+class hash_func {
+public:
+
+    unsigned long operator()(const string& key); //hash function from string to unsigned long
+    //to be implemented
+
+}; // class hash_func
+
+
+class HashMapDriver {
+protected:
+
+    fstream inFile; //for processing the dictionary file
+
+    hash_map<string, string, hash_func> dict; //the dictionary hash map
+    //key:string, value:string
+    //hash_func:string to unsigned long
+
+public:
+
+    // Postcondition: The input file has been created
+    //                with their names read in from the keyboard.
+    void setUpFiles();
+
+    // Postcondition: All the words in the dictionary file have been read in and processed.
+    void readAndProcess();
+
+    void testDictionary(); //test the dictionary
+
+}; // class HashMapDriver
